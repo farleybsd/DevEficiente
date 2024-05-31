8a193bd9df4f9dcf6caf114e999efb551fe440e5
farleybsd <farley.t.i@hotmail.com>
2024-05-31 11:37:50 -0300

    Teste Handler

---
+++ MercadoLivre/backend/src/br.com.deveficiente.mercadolivre/Tests/br.com.deveficiente.mercadolivre.Tests/CommandHandler/UserCreate/UserCreateCommandHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using br.com.deveficiente.mercadolivre.Application.CommandHandler;
+using br.com.deveficiente.mercadolivre.Domain.Interfaces.Repository;
+using br.com.deveficiente.mercadolivre.Domain.Interfaces.UnitOfWork;
+using br.com.deveficiente.mercadolivre.Domain.NotificationObjects;
+using Moq;
+
+namespace br.com.deveficiente.mercadolivre.Tests.CommandHandler.UserCreate
+{
+    public class UserCreateCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_SuccessfulCreation_ReturnsSucceededResult()
+        {
+            // Arrange
+            var unitOffWorkMock = new Mock<IUnitOfWork>();
+            var loginRepositoryMock = new Mock<ILoginRepository>();
+            unitOffWorkMock.Setup( uow => uow.LoginRepository).Returns(loginRepositoryMock.Object);
+
+            var notificationContext = new NotificationContext();
+            var handler = new UserCreateCommandHandler(unitOffWorkMock.Object,notificationContext);
+
+            var command = new  UserCreateCommandHandlerTestsDataBuilders()
+                 .WithEmail("test@example.com")
+                  .WithPassword("password123")
+                  .Build();
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.Succeeded);
+            Assert.NotNull(result.Data);
+            Assert.Equal("test@example.com", result.Data.Email);
+            Assert.Equal("password123", result.Data.Password);
+
+        }
+
+        [Fact]
+        public async Task Handle_ExceptionThrown_ReturnsFailedResultAndAddsNotification()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var loginRepositoryMock = new Mock<ILoginRepository>();
+            unitOfWorkMock.Setup(uow => uow.LoginRepository).Returns(loginRepositoryMock.Object);
+            unitOfWorkMock.Setup(uow => uow.Commit()).Throws(new Exception("Database error"));
+
+            var notificationContext = new NotificationContext();
+
+            var handler = new UserCreateCommandHandler(unitOfWorkMock.Object, notificationContext);
+
+            var command = new UserCreateCommandHandlerTestsDataBuilders()
+                 .WithEmail("test@example.com")
+                  .WithPassword("password123")
+                  .Build();
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.Succeeded);
+            Assert.Null(result.Data);
+            Assert.Contains("Erro ao Salvar um Login", result.Errors);
+            Assert.True(notificationContext.HasNotifications);
+        }
+    }
+}
+
---
+++ MercadoLivre/backend/src/br.com.deveficiente.mercadolivre/Tests/br.com.deveficiente.mercadolivre.Tests/CommandHandler/UserCreate/UserCreateCommandHandlerTestsDataBuilders.cs
@@ -0,0 +1,33 @@
+﻿using br.com.deveficiente.mercadolivre.Application.Commands;
+
+namespace br.com.deveficiente.mercadolivre.Tests.CommandHandler.UserCreate
+{
+    public class UserCreateCommandHandlerTestsDataBuilders
+    {
+        private string Email;
+        private string PassWord;
+
+        public UserCreateCommandHandlerTestsDataBuilders()
+        {
+            Email = "test@example.com";
+            PassWord = "password123";
+        }
+
+        public UserCreateCommandHandlerTestsDataBuilders WithEmail (string email)
+        {
+            Email = email;
+            return this;
+        }
+
+        public UserCreateCommandHandlerTestsDataBuilders WithPassword(string password)
+        {
+            PassWord = password;
+            return this;
+        }
+
+        public UserCreateCommand Build()
+        {
+            return new UserCreateCommand(Email, PassWord);
+        }
+    }
+}
